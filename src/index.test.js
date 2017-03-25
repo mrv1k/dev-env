bd88c50ac55e1e11ab69c21f21f06820ae7de91e
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2017-03-24 23:53:19 -0400

    Actually fix broken tests

--- src/index.test.js
+++ src/index.test.js
@@ -5,15 +5,15 @@ import fs from 'fs'; //filesystem
 describe('Our first test', function() {
   it('should pass', function(){
     expect(true).to.equal(true);
-  })
-})
+  });
+});
 
 describe('index.html', () => {
   it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello Pituh!?");
+      expect(h1.innerHTML).to.equal("Hello Pituh!");
       done();
       window.close();
     });
