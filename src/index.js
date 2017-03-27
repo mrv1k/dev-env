a65133748fedc8f990dbb757f64ed91961bea96b
Viktor Khotimchenko <vkhotimchenko@gmail.com>
2017-03-26 20:14:35 -0400

    HTTP Calls using fetch, Mocking and HTTP using Schema, Manipulating Data using JSON server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import './index.css';
+// import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
-import numeral from 'numeral';
+// populate table of users via API call
+getUsers().then(result => {
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">NAHOOI S PLYAJA</a></td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>
+    `
+  });
 
-/* eslint-disable no-console */
+  global.document.getElementById('users').innerHTML = usersBody;
 
-const courseValue = numeral(322).format('$0,0.00');
+  const deleteLinks = global.document.getElementsByClassName('deleteUser')
 
-console.log(`I would pay ${courseValue} for this ok course!`);
+  // Must user array.form to create a real array from a DOM colleciton
+  // only getElemsByClassName returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    }
+  });
+});
